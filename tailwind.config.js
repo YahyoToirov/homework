735efeaf50f3697fe2f59bde838430bdf143a9e5
YahyoToirov <yahyotoirov7@gmail.com>
2024-09-05 13:28:52 +0500

    last

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./*.html", "./src/**/*.html"],
+  theme: {
+    extend: {},
+    colors: {
+      navy_b: ["#18216D"],
+    },
+  },
+  plugins: [],
+};
